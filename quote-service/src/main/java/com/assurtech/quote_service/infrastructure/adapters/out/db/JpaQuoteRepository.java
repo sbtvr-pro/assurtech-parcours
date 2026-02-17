5f8303518f03fc8fb9011e08ead04be3b2a78c0a
Stephane Btvr <stephane.bettaver@randstaddigital.com>
2026-02-17 17:49:36 +0100

    fix: Change primary key type from UUID to Long in JpaQuoteRepository

--- quote-service/src/main/java/com/assurtech/quote_service/infrastructure/adapters/out/db/JpaQuoteRepository.java
+++ quote-service/src/main/java/com/assurtech/quote_service/infrastructure/adapters/out/db/JpaQuoteRepository.java
@@ -1,9 +1,8 @@
 package com.assurtech.quote_service.infrastructure.adapters.out.db;
 
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaQuoteRepository extends  JpaRepository<QuoteEntity, UUID> {
+public interface JpaQuoteRepository extends  JpaRepository<QuoteEntity, Long> {
     
 }
